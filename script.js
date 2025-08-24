a3633102cdb7210711d5a04243ab1ec919caefcf
Marame <bentabidou@gmail.com>
2025-08-24 11:04:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const imageInput = document.getElementById('imageInput');
 const moodBoard = document.getElementById('moodBoard');
-const downloadBtn = document.getElementById('downloadBoard');
 
-// Upload images to mood board
 imageInput.addEventListener('change', (e) => {
   const files = e.target.files;
   for (let file of files) {
@@ -16,17 +14,6 @@ imageInput.addEventListener('change', (e) => {
   }
 });
 
-// Download mood board as single image
-downloadBtn.addEventListener('click', () => {
-  if (moodBoard.children.length === 0) {
-    alert("Please add at least one image before downloading.");
-    return;
-  }
-  html2canvas(moodBoard).then(canvas => {
-    const link = document.createElement('a');
-    link.download = 'moodboard.png';
-    link.href = canvas.toDataURL('image/png');
-    link.click();
-  });
+document.getElementById('downloadBoard').addEventListener('click', () => {
+  alert("Download feature coming soon! For now, right-click on images to save.");
 });
-
